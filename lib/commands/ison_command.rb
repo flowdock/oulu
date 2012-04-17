c70bcf7b63be8164c78dd5df17ce2c0b198ab1d5
Tuomas Silen <tuomas.silen@nodeta.fi>
2012-04-17 17:13:34 +0300

    Support ISON command for NickServ

---
+++ lib/commands/ison_command.rb
@@ -0,0 +1,28 @@
+class IsonCommand < Command
+  register_command :ISON
+
+  def set_data(args)
+    @data = args
+  end
+
+  def valid?
+    !!@data
+  end
+
+  def execute!
+    available_nicks = []
+    ison_nicks = @data.map{|n| n.downcase}
+    $logger.debug("ison: executing: #{ison_nicks.inspect}")
+    if !@authenticated and ison_nicks.include? 'nickserv'
+      available_nicks << "nickserv"
+    else
+#      irc_connection.channels.values.each do |channel|
+#        channel_nicks = channel.users.map { |u| u.nick.downcase }
+#        available_nicks |= (channel_nicks & ison_nicks )
+#      end
+    end
+    reply = render_ison(available_nicks)
+
+    send_reply(reply)
+  end
+end
