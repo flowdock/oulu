f95ff65a59c644aabfec53f2abe567f1fc65d32a
Arttu Tervo <arttu.tervo@gmail.com>
2012-04-18 11:38:15 +0300

    Removed unnecessary valid? from StatusEvent.

--- lib/events/status_event.rb
+++ lib/events/status_event.rb
@@ -2,10 +2,6 @@ class StatusEvent < FlowdockEvent
   register_event "status"
   register_event "line"
 
-  def valid?
-    !@message['content'].nil?
-  end
-
   def process
     text = cmd.send(:"render_#{@message['event']}", @user.irc_host, @channel.irc_id, @message['content'])
     @irc_connection.send_reply(text)
