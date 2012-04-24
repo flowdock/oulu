7414eb38cca37507ed5b7e778db048e0bebb66bb
Otto Hilska <otto@flowdock.com>
2012-04-24 15:25:22 +0300

    Fixed whitespace.

--- lib/events/user_activity_event.rb
+++ lib/events/user_activity_event.rb
@@ -1,6 +1,5 @@
 class UserActivityEvent < FlowdockEvent
   register_event "activity.user"
-    
 
   # We get the activity for each flow separately, but in irc it's global
   def process
@@ -10,6 +9,5 @@ class UserActivityEvent < FlowdockEvent
       channel_user = channel.find_user_by_id(@user.id)
       channel_user.update_last_activity(@message['sent']) if channel_user
     end
-
   end
 end
