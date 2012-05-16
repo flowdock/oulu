221edd4582a1798c68b3e7ded3c893bf5697592b
Otto Hilska <otto@flowdock.com>
2012-05-15 17:56:52 -0700

    AWAY command.

---
+++ spec/commands/away_command_spec.rb
@@ -0,0 +1,25 @@
+describe AwayCommand do
+  it "should set IRC connection's AWAY message" do
+    irc_connection = mock(:irc_connection, :authenticated? => true,
+      :nick => 'Otto')
+    irc_connection.should_receive(:send_reply).with(/You have been marked as being away/)
+    irc_connection.should_receive(:set_away).with("gone somewhere, brb")
+
+    cmd = AwayCommand.new(irc_connection)
+    cmd.set_data(["gone somewhere, brb"])
+    cmd.valid?.should be_true
+    cmd.execute!
+  end
+
+  it "should unset IRC connection's AWAY message" do
+    irc_connection = mock(:irc_connection, :authenticated? => true,
+      :nick => 'Otto')
+    irc_connection.should_receive(:send_reply).with(/You are no longer marked as being away/)
+    irc_connection.should_receive(:set_away).with(nil)
+
+    cmd = AwayCommand.new(irc_connection)
+    cmd.set_data([])
+    cmd.valid?.should be_true
+    cmd.execute!
+  end
+end
