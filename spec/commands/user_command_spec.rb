051ce451c1416c6665f17da665945a9a69553b43
Tuomas Silen <tuomas.silen@nodeta.fi>
2012-06-20 19:47:18 +0300

    Validate USER command

--- spec/commands/user_command_spec.rb
+++ spec/commands/user_command_spec.rb
@@ -1,8 +1,21 @@
 require 'spec_helper'
 
 describe UserCommand do
-  it "shouldn't be valid if user is already authenticated" do
-    irc_connection = mock(:irc_connection, :authenticated? => true)
+  it "should disconnect user if username is invalid" do
+    irc_connection = mock(:irc_connection, :authenticated? => false, :nick => "otto", :email => nil, :registered? => false)
+    irc_connection.should_not_receive(:email=)
+    irc_connection.should_receive(:send_reply).with(/ERROR :Closing Link:/)
+    irc_connection.should_not_receive(:send_reply).with(/PING/)
+    irc_connection.should_receive(:quit!)
+
+    cmd = UserCommand.new(irc_connection)
+    cmd.set_data(["ot to", "foo", "bar", "Otto Hilska"])
+    cmd.valid?.should be_true
+    cmd.execute!
+  end
+
+  it "shouldn't be valid if user is already registered" do
+    irc_connection = mock(:irc_connection, :authenticated? => false, :nick => "otto", :registered? => true)
 
     cmd = UserCommand.new(irc_connection)
     cmd.set_data(["otto", "foo", "bar", "Otto Hilska"])
@@ -10,11 +23,12 @@ describe UserCommand do
   end
 
   it "should configure email and real name, and send a PING" do
-    irc_connection = mock(:irc_connection, :authenticated? => false)
+    irc_connection = mock(:irc_connection, :authenticated? => false, :nick => "otto", :registered? => false, :last_ping_sent => nil)
     irc_connection.should_receive(:email=).with("otto@unknown")
     irc_connection.should_receive(:real_name=).with("Otto Hilska")
     irc_connection.should_receive(:last_ping_sent=).with(/FLOWDOCK-/)
     irc_connection.should_receive(:send_reply).with(/PING/)
+    irc_connection.should_receive(:registered?).exactly(2).times.and_return(false, true)
 
     cmd = UserCommand.new(irc_connection)
     cmd.set_data(["otto", "foo", "bar", "Otto Hilska"])
