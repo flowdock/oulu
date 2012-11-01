23075db6e4b93c853d078abd6f0d77866850c81d
Otto Hilska <otto@flowdock.com>
2012-11-01 11:12:19 +0200

    Better MOTD.

--- lib/commands/pong_command.rb
+++ lib/commands/pong_command.rb
@@ -24,7 +24,13 @@ class PongCommand < Command
 
   def motd_lines
     [render_welcome, render_yourhost, render_created, render_motd_start,
-      render_motd_line("For instructions, check out https://www.flowdock.com/help"),
+      render_motd_line("For instructions, check out https://www.flowdock.com/help/irc"),
+      render_motd_line(""),
+      render_motd_line("This server software is open source, and your contributions are"),
+      render_motd_line("welcome. See https://github.com/flowdock/oulu"),
+      render_motd_line(""),
+      render_motd_line("Flowdock is in active development, and any feedback is appreciated."),
+      render_motd_line("You email us at team@flowdock.com."),
       render_motd_end]
   end
 
