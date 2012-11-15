9c455de4f204b878e731be137c22fb97c15c30c7
Tuomas Silen <tuomas.silen@nodeta.fi>
2012-11-15 22:12:19 +0200

    fix ison and support private messages

--- lib/commands/ison_command.rb
+++ lib/commands/ison_command.rb
@@ -13,8 +13,11 @@ class IsonCommand < Command
     available_nicks = []
     ison_nicks = @data.map{|n| n.downcase}
     if !authenticated? and ison_nicks.include? 'nickserv'
-      available_nicks << "nickserv"
+      available_nicks << "NickServ"
+    elsif authenticated?
+      available_nicks = irc_connection.unique_users.map{|u| u.nick}.select{|n| ison_nicks.include? n.downcase}
     end
+
     reply = render_ison(available_nicks)
 
     send_reply(reply)
