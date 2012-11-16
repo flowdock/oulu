515a4c7e1eafc10d164c4bb9aeb18078264f5e3c
Otto Hilska <otto@flowdock.com>
2012-11-16 14:58:31 -0800

    Use newly added find_user.

--- lib/commands/whois_command.rb
+++ lib/commands/whois_command.rb
@@ -15,7 +15,7 @@ class WhoisCommand < Command
       render_whois("NickServ", IrcServer::NICKSERV_EMAIL, IrcServer::NICKSERV_NAME, 0, IrcServer::CREATED_AT)
     elsif @nick == user_nick.downcase # myself, this works even before authentication
       render_whois(user_nick, user_email, user_real_name, 0, IrcServer::CREATED_AT)
-    elsif user = irc_connection.find_user_by_nick(@nick)
+    elsif user = find_user(@nick)
       render_whois(user.nick, user.email, user.name, user.idle_time, IrcServer::CREATED_AT)
     else
       render_no_such_nick(@nick)
