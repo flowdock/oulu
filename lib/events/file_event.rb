05e7010f23188b382920dfeef008f14afefa8d0f
Kevin Pulo <kevin.pulo@mongodb.com>
2015-01-20 12:02:54 +1100

    Properly URI-encode uploaded file URLs.
    Fixes #60.

--- lib/events/file_event.rb
+++ lib/events/file_event.rb
@@ -1,9 +1,11 @@
+require 'uri'
+
 class FileEvent < FlowdockEvent
   register_event "file"
 
   def render
     (organization, flow) = @target.visible_name.split('/')
-    url = "https://www.#{IrcServer::FLOWDOCK_DOMAIN}/rest#{@message['content']['path']}"
+    url = "https://www.#{IrcServer::FLOWDOCK_DOMAIN}/rest#{URI::encode(@message['content']['path'])}"
     render_privmsg(@user.irc_host, @target.irc_id, url)
   end
 
